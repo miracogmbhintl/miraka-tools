73012fde2dd6102499e2cb90b60510b163829cf7
Miraka & Co <office@miraka.ch>
2026-03-27 17:28:36 +0000

    Update 404 page to navigate back to previous page instead of always going home

--- src/components/NotFoundWrapper.tsx
+++ src/components/NotFoundWrapper.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import { DevLinkProvider } from '../site-components/DevLinkProvider';
-import { NotFound } from '../site-components/NotFound';
+import Block from '../site-components/_Builtin/Block';
+import Heading from '../site-components/_Builtin/Heading';
+import { baseUrl } from '../lib/base-url';
 
 export default function NotFoundWrapper() {
+  const handleGoBack = () => {
+    // Check if there's history to go back to
+    if (window.history.length > 1) {
+      window.history.back();
+    } else {
+      // If no history, go to home
+      window.location.href = baseUrl + '/';
+    }
+  };
+
   return (
     <DevLinkProvider>
-      <NotFound />
+      <Block className="utility-page-wrap" tag="div">
+        <Block className="utility-page-content" tag="div">
+          <Heading className="text---heading" tag="h2">
+            404
+          </Heading>
+          <Block tag="div">
+            Oops! Nothing is here.
+          </Block>
+          <button
+            onClick={handleGoBack}
+            style={{
+              background: 'transparent',
+              border: 'none',
+              padding: 0,
+              cursor: 'pointer',
+              font: 'inherit',
+              display: 'inline-block',
+              marginTop: '20px'
+            }}
+          >
+            <Block className="back-to-home-button" tag="div">
+              {'<-- Back to Previous Page'}
+            </Block>
+          </button>
+        </Block>
+      </Block>
     </DevLinkProvider>
   );
 }
